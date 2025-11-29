725e9967cb98547c9ad0ae4892dd2e0eb8be736d
Fares <faresahmed3090@gmail.com>
2025-11-29 23:42:36 +0200

    Add Discount service with gRPC support and database context

---
+++ src/Services/Discount/Discount.Grpc/Data/Extensions.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+
+public static class Extensions
+{
+    public static async Task<IApplicationBuilder> UseMigration(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+
+        await dbContext.Database.MigrateAsync();
+
+        return app;
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,8 @@
+using Discount.Grpc.Data;
+
+namespace Discount.Grpc.Services;
+
+public class DiscountService(DiscountContext dbContext, ILogger<DiscountService> logger)
+    : DiscountProtoService.DiscountProtoServiceBase
+{
+}
