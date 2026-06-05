78037d09d1eb3e924fc16f066c9492d128bdbbb9
Fares <faresahmed3090@gmail.com>
2026-06-05 19:36:13 +0300

    Implement ApplicationDbContext with IApplicationDbContext interface, add database connection string, and create initial migration for ordering system

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,12 +1,13 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddApiServices()
-    .AddApplicationServices()
+    .AddApplicationServices(builder.Configuration)
     .AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
@@ -14,4 +15,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseApiServices();
+
+if (app.Environment.IsDevelopment()) await app.InitialiseDatabaseAsync();
 app.Run();
--- src/Services/Ordering/Ordering.Domain/Abstractions/IAggregate.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IAggregate.cs
@@ -2,11 +2,10 @@ namespace Ordering.Domain.Abstractions;
 
 public interface IAggregate : IEntity
 {
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+    IDomainEvent[] ClearDomainEvents();
 }
 
 public interface IAggregate<T> : IAggregate, IEntity<T>
 {
-    IReadOnlyList<IDomainEvent> DomainEvents { get; }
-
-    IDomainEvent[] ClearDomainEvents();
 }
